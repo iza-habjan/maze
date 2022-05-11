13f6af1b31da9b1e818e2aca7a6fa0463a77957b
Iza Habjan <iza.habjan@gmail.com>
2022-05-11 10:41:28 +0200

    comments

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const { Engine, Render, Runner, World, Bodies } = Matter;
 // The Object.create() method creates a new object, using an existing object as the prototype of the newly created object.
 const engine = Engine.create();
 // world object gets created along with the Engine object; world is comming from engine
+// world varible contains a representation of all the different shapes inside of World
 const { world } = engine;
+// creating a canvas element
 const render = Render.create({
   element: document.body,
   engine: engine,
