a28cb722b28d254d659c941b42850774780322cd
Iza Habjan <iza.habjan@gmail.com>
2022-05-23 11:11:00 +0200

    guiding comments

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { Engine, Render, Runner, World, Bodies } = Matter;
 
-const cells = 5;
+const cells = 3;
 const width = 600;
 const height = 600;
 
@@ -45,3 +45,22 @@ const verticals = Array(cells)
 const horizontals = Array(cells - 1)
   .fill(null)
   .map(() => Array(cells).fill(false));
+
+// Starting Point
+
+const startRow = Math.floor(Math.random() * cells);
+const startColumn = Math.floor(Math.random() * cells);
+console.log(startRow, startColumn);
+
+const stepThroughCell = (row, column) => {
+  // If I have visited the cell at [row, column], then return
+  // Mark this cell as being visited
+  // Assemble randomly-ordered list of neighbors
+  // For each neighbor...
+  // See if that neighbor is out of bounds
+  // If we have visited that neighbor continue to next neighbor
+  // Remove a wall from either horizontals or verticals
+  // Visit that next cell
+};
+
+stepThroughCell(startRow, startColumn);
