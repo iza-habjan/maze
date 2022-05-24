e646aa898a9378eab77df32efa732ff6e1d2f740
Iza Habjan <iza.habjan@gmail.com>
2022-05-24 11:07:46 +0200

    detecting a win

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { Engine, Render, Runner, World, Bodies, Body } = Matter;
+const { Engine, Render, Runner, World, Bodies, Body, Events } = Matter;
 
 const cells = 3;
 const width = 600;
@@ -166,6 +166,7 @@ const goal = Bodies.rectangle(
   unitLength * 0.7,
   unitLength * 0.7,
   {
+    label: "goal",
     isStatic: true,
   }
 );
@@ -173,7 +174,9 @@ World.add(world, goal);
 
 // Ball
 
-const ball = Bodies.circle(unitLength / 2, unitLength / 2, unitLength / 4);
+const ball = Bodies.circle(unitLength / 2, unitLength / 2, unitLength / 4, {
+  label: "ball",
+});
 World.add(world, ball);
 
 document.addEventListener("keydown", (event) => {
@@ -194,4 +197,19 @@ document.addEventListener("keydown", (event) => {
   if (event.keyCode === 37) {
     Body.setVelocity(ball, { x: x - 5, y });
   }
+});
+
+// Win Condition
+
+Events.on(engine, "collisionStart", (event) => {
+  event.pairs.forEach((collision) => {
+    const labels = ["ball", "goal"];
+
+    if (
+      labels.includes(collision.bodyA.label) &&
+      labels.includes(collision.bodyB.label)
+    ) {
+      console.log("User Won!");
+    }
+  });
 });
